4b0a95ad6a70aa7d122a51cb949396ebcf99c220
beerybobb <nystudio13@gmail.com>
2014-12-06 16:39:42 -0500

    brand menu

---
+++ includes/brand-menu.php
@@ -0,0 +1,16 @@
+<!-- Bottom menu element-->
+<nav class="cbp-spmenu cbp-spmenu-horizontal cbp-spmenu-bottom">
+	<h3>Menu</h3>
+	<a href="#">Celery seakale</a>
+	<a href="#">Dulse daikon</a>
+	<a href="#">Zucchini garlic</a>
+	<a href="#">Catsear azuki bean</a>
+	<a href="#">Dandelion bunya</a>
+	<a href="#">Rutabaga</a>
+	<a href="#">Celery seakale</a>
+	<a href="#">Dulse daikon</a>
+	<a href="#">Zucchini garlic</a>
+	<a href="#">Catsear azuki bean</a>
+	<a href="#">Dandelion bunya</a>
+	<a href="#">Rutabaga</a>
+</nav>
