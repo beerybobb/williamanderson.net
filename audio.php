75302f0764099c7dcc9909941feeff11e0f4a743
beerybobb <nystudio13@gmail.com>
2014-12-05 23:08:46 -0500

    32

--- audio.php
+++ audio.php
@@ -3,10 +3,9 @@
 <head>
 <link href='http://fonts.googleapis.com/css?family=Open+Sans' rel='stylesheet' type='text/css'>
 <script src="lightbox/js/jquery-1.11.0.min.js"></script>
-	<script src="lightbox/js/lightbox.min.js"></script>
-    <link href="lightbox/css/lightbox.css" rel="stylesheet" />
-    <link href="css/style.css" rel="stylesheet" />
-
+<script src="lightbox/js/lightbox.min.js"></script>
+<link href="lightbox/css/lightbox.css" rel="stylesheet" />
+<link href="css/style.css" rel="stylesheet" />
 <meta charset="utf-8">
 <title>Audio | Tracking, Mixing, Editing, Mastering</title>
 </head>
@@ -14,17 +13,22 @@
 <body>
 <?php include "includes/header.php" ?>
 <div class="container"></div>
-<div class="box effect2" style="width:1200px; height:333px"><span class="banner-text blue1">Audio Mixing, Editing, Mastering</span><img style="padding-bottom:40px;" src="images/audio-temp.jpg" width="1200" height="333"  alt=""/>
-          <span> I've done lots of things. hire me. I'll make all your wildest dreams come true, at reasonable prices. jeez i need money. help me out yo. awwwwww shit shawy, i got equiptment and stuff ya hurr? ok i need real copy here.</span>
+<div class="box effect2" >
+<span class="banner-text blue1">Audio Mixing, Editing, Mastering</span><img style="padding-bottom:40px;" src="images/audio-temp.jpg" width="1200" height="333"  alt="Audio Mixing, Editing, and Mastering"/> <span> I've done lots of things. hire me. I'll make all your wildest dreams come true, at reasonable prices. jeez i need money. help me out yo. awwwwww shit shawy, i got equiptment and stuff ya hurr? ok i need real copy here.</span>
 <h2>Junk I've Done</h2>
-<div class="audio-box"><div><div class="effect3"><img src="images/audio/stimpy-lockjaw.jpg"></div><span>Stimpy Lockjaw</span></div>
-<div><div class="effect3"><img src="images/audio/ever-forthright.jpg"></div><span>Ever Forthright</span></div>
-<div><div class="effect3"><img src="images/audio/painted-in-exile.jpg"></div><span>Painted In Exile</span></div><div><div class="effect3"><img src="images/audio/backtrack.jpg"></div><span>Backtrack</span></div>
-
+<div class="audio-box">
+  <div>
+    <div class="effect3"><img src="images/audio/stimpy-lockjaw.jpg"></div>
+    <span>Stimpy Lockjaw</span></div>
+  <div>
+    <div class="effect3"><img src="images/audio/ever-forthright.jpg"></div>
+    <span>Ever Forthright</span></div>
+  <div>
+    <div class="effect3"><img src="images/audio/painted-in-exile.jpg"></div>
+    <span>Painted In Exile</span></div>
+  <div>
+    <div class="effect3"><img src="images/audio/backtrack.jpg"></div>
+    <span>Backtrack</span></div>
 </div>
-
-
-
-
 </body>
 </html>
